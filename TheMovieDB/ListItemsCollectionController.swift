c81c8d64a1deae91f10503f0f6494d4868e7804a
Artsiom Grintsevich <greenfrvr@gmail.com>
2015-12-03 14:10:40 +0300

    collections improvement

--- TheMovieDB/ListItemsCollectionController.swift
+++ TheMovieDB/ListItemsCollectionController.swift
@@ -13,6 +13,14 @@ class ListItemsCollectionController: UICollectionViewController, ListItemsCollec
     
     var items = [CompilationItem]()
     
+    override func viewDidLoad() {
+        adjustCollectionFlow(UIScreen.mainScreen().bounds.size)
+    }
+
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        adjustCollectionFlow(size)
+    }
+    
     override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         let item = items[indexPath.row]
         MovieDetailsController.presentControllerWithNavigation(self, id: String(item.itemId!))
@@ -22,9 +30,8 @@ class ListItemsCollectionController: UICollectionViewController, ListItemsCollec
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(ListItemViewCell.identifier, forIndexPath: indexPath) as! ListItemViewCell
         let item = items[indexPath.row]
         
-        cell.movieNameLabel.padding = 10
         cell.movieNameLabel.text = item.title
-        cell.posterImageView.sd_setImageWithURL(NSURL(posterPath: item.posterPath, size: 3), placeholderImage: UIImage(res: .Placeholder))
+        cell.posterImageView.sd_setImageWithURL(NSURL(posterPath: item.posterPath, size: 3), placeholderImage: UIImage(res: .PosterPlaceholder))
         
         return cell
     }
@@ -40,6 +47,25 @@ class ListItemsCollectionController: UICollectionViewController, ListItemsCollec
     func collectionFetched(collection: [CompilationItem]) {
         items += collection
         collectionView?.reloadData()
+    }
+    
+    func adjustCollectionFlow(size: CGSize) {
+        func itemSize(isPortrait: Bool) -> CGSize {
+            let cellWidth = size.width / CGFloat(isPortrait ? 3 : 5)
+            
+            return CGSizeMake(cellWidth, cellWidth / 2 * 3)
+        }
+        
+        let isPortrait = size.width < size.height
+        let topInset = CGFloat(isPortrait ? 22 : 56)
+        
+        let layout = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.sectionInset = UIEdgeInsetsMake(topInset, 0, 0, 0)
+        layout.itemSize = itemSize(isPortrait)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        
+        collectionView?.collectionViewLayout = layout
     }
 }
 
